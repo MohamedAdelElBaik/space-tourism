43ecda830cd2b2f0ce3cf4d5180726c2d9dcbc8a
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-12-15 14:56:32 +0200

    create page structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import "./index.css";
+import logo from "./images/group-2.svg";
+// import Nav from "./components/Nav";
+import { useState } from "react";
+// import HomeContent from "./components/HomeContent";
+
 function App() {
+  // const [navResult, setNavResult] = useState("home");
+
   return (
-    <div className="App">
-      <header className="App-header">hello there</header>
+    <div className="app">
+      <Header>
+        <Logo />
+        {/* <Nav setNavResult={setNavResult} /> */}
+      </Header>
+      {/* {navResult === "home" && <HomeContent />} */}
+    </div>
+  );
+}
+
+function Header({ children }) {
+  return (
+    <header
+      className="app-header flex"
+      style={{ justifyContent: "space-between", alignItems: "center" }}
+    >
+      {children}
+    </header>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <img className="nav--logo" src={logo} alt="logo"></img>
     </div>
   );
 }
