8faae7b784a6181d00f695e928bf4987eb8f8637
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-12-16 22:42:28 +0200

    change Home and Destination import

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import "./index.css";
 import logo from "./images/group-2.svg";
 import Nav from "./components/Nav";
 import { useState } from "react";
-import HomeContent from "./components/HomeContent";
-import DestinationContent from "./components/Destination/Destination";
+import Home from "./components/Home";
+import Destination from "./components/Destination/Destination";
 
 function App() {
-  const [navResult, setNavResult] = useState("home");
+  const [navResult, setNavResult] = useState("destination");
+
+  let device = "desktop";
+  // const windowWidth = Number(window.innerWidth);
+  // let device = "mobile";
+  // if (windowWidth >= 600) device = "tablet";
+  // if (windowWidth >= 800) device = "desktop";
+
+  const style = {
+    backgroundImage: `url(./assets/${navResult}/${navResult}-background-${device}.svg)`,
+  };
 
   return (
-    <div className="app">
+    <div className="app" style={style}>
       <Header>
         <Logo />
         <Nav navResult={navResult} setNavResult={setNavResult} />
       </Header>
-      <Section>
-        {navResult === "home" && <HomeContent />}
-        {navResult === "destination" && <DestinationContent />}
+      <Section className={navResult}>
+        {navResult === "home" && <Home />}
+        {navResult === "destination" && <Destination />}
         {navResult === "crew" && <h1>Not Yet</h1>}
         {navResult === "technology" && <h1>Not Yet</h1>}
       </Section>
@@ -36,8 +46,15 @@ function Logo() {
   );
 }
 
-function Section({ children }) {
-  return <section className="app--content">{children}</section>;
+function Section({ children, className }) {
+  return (
+    <section
+      className={`app--content ${className} flow`}
+      style={{ "--flow-space": "2em" }}
+    >
+      {children}
+    </section>
+  );
 }
 
 export default App;
